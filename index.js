2bb1f32081e9bb3e74a59f93b97ddd2688dcdeb0
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-02-13 18:56:28 -0600

    initial setup commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { StacheElement } from "can";
+import view from "./app.stache";
+
+class MyApp extends StacheElement {
+  static view = view;
+
+  static props = {
+    message: {
+      default: "Hello World"
+    }
+  };
+};
+customElements.define("my-app", MyApp);
