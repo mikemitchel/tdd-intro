e156d20e1cacb456c18dacffd4bd60fc039f3ad2
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2020-02-19 13:41:49 -0600

    use connected() and fix spacing

--- note-panel/note-panel.js
+++ note-panel/note-panel.js
@@ -13,30 +13,30 @@ export default class NotePanel extends StacheElement {
     }
   }
 
-    setActive(note) {
-      console.log('Activate Note ', note.id)
-      // clear any active notes
-      this.notesList.forEach((note) => {
-        note.isActive = false
-      })
-      // activate clicked note
-      note.isActive = true
-      // set focus on matching sticky note
-      document.getElementById(`sticky-note-${note.id}`).focus()
-    }
+  setActive(note) {
+    console.log('Activate Note ', note.id)
+    // clear any active notes
+    this.notesList.forEach((note) => {
+      note.isActive = false
+    })
+    // activate clicked note
+    note.isActive = true
+    // set focus on matching sticky note
+    document.getElementById(`sticky-note-${note.id}`).focus()
+  }
 
-    connectedCallback() {
-      // load some sample notes
-      this.notesList.assign([
-        { title: "write more tests!", content: "2020, the year of best practices", isActive: false, id: 1 },
-        { title: "call mom", content: "ask her where my original nintendo is", isActive: false, id: 2 },
-        { title: "make tacos", content: "pick up guacamole too", isActive: false, id: 3 },
-        { title: "learn canjs", content: "CanJS Version 6 is out using custom elements!", isActive: false, id: 4 },
-        { title: "high five someone", content: "random acts of confusion", isActive: false, id: 5 },
-        { title: "netflix and chill", content: "Mind of Chef, season 1 is amazing", isActive: false, id: 6 },
-        { title: "test comfort zone", content: "Ask 3 dumb questions today", isActive: false, id: 7 }
-      ])
-    }
+  connected() {
+    // load some sample notes
+    this.notesList.assign([
+      { title: "write more tests!", content: "2020, the year of best practices", isActive: false, id: 1 },
+      { title: "call mom", content: "ask her where my original nintendo is", isActive: false, id: 2 },
+      { title: "make tacos", content: "pick up guacamole too", isActive: false, id: 3 },
+      { title: "learn canjs", content: "CanJS Version 6 is out using custom elements!", isActive: false, id: 4 },
+      { title: "high five someone", content: "random acts of confusion", isActive: false, id: 5 },
+      { title: "netflix and chill", content: "Mind of a Chef, season 1 is amazing", isActive: false, id: 6 },
+      { title: "test comfort zone", content: "Ask 3 dumb questions today", isActive: false, id: 7 }
+    ])
+  }
 }
 
 customElements.define("note-panel", NotePanel)
