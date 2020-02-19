16c1faa91a49cbf4738a8920685e6ed1acda8f82
mitch <dev@mukungfu.com>
2020-02-18 22:38:46 -0600

    assign sample notes in connectedCallback

--- note-panel/note-panel.js
+++ note-panel/note-panel.js
@@ -2,23 +2,16 @@ import { StacheElement } from "can"
 import view from "./note-panel.stache"
 import { NoteList } from "~/models/note"
 
-class NotePanel extends StacheElement {
+export default class NotePanel extends StacheElement {
   static view = view
 
   static props = {
     notesList: {
       get default() {
-        return new NoteList([
-          { title: "write more tests!", content: "2020, the year of best practices", isActive: false, id: 1 },
-          { title: "call mom", content: "ask her where my original nintendo is", isActive: false, id: 2 },
-          { title: "make tacos", content: "pick up guacamole too", isActive: false, id: 3 },
-          { title: "learn canjs", content: "CanJS Version 6 is out using custom elements!", isActive: false, id: 4 },
-          { title: "high five someone", content: "random acts of confusion", isActive: false, id: 5 },
-          { title: "netflix and chill", content: "Mind of Chef, season 1 is amazing", isActive: false, id: 6 },
-          { title: "test comfort zone", content: "Ask 3 dumb questions today", isActive: false, id: 7 }
-        ]);
+        return new NoteList();
       }
-    },
+    }
+  }
 
     setActive(note) {
       console.log('Activate Note ', note.id)
@@ -31,6 +24,19 @@ class NotePanel extends StacheElement {
       // set focus on matching sticky note
       document.getElementById(`sticky-note-${note.id}`).focus()
     }
-  }
+
+    connectedCallback() {
+      // load some sample notes
+      this.notesList.assign([
+        { title: "write more tests!", content: "2020, the year of best practices", isActive: false, id: 1 },
+        { title: "call mom", content: "ask her where my original nintendo is", isActive: false, id: 2 },
+        { title: "make tacos", content: "pick up guacamole too", isActive: false, id: 3 },
+        { title: "learn canjs", content: "CanJS Version 6 is out using custom elements!", isActive: false, id: 4 },
+        { title: "high five someone", content: "random acts of confusion", isActive: false, id: 5 },
+        { title: "netflix and chill", content: "Mind of Chef, season 1 is amazing", isActive: false, id: 6 },
+        { title: "test comfort zone", content: "Ask 3 dumb questions today", isActive: false, id: 7 }
+      ])
+    }
 }
+
 customElements.define("note-panel", NotePanel)
